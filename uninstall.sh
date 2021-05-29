16c5b7d43804245ec472fdabd25a7f21118c8e1d
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2021-05-28 23:06:52 -0400

    show some output

--- uninstall.sh
+++ uninstall.sh
@@ -7,18 +7,23 @@
 # License : WTFPLv2                                              \          /  #
 #------------------------------------------------------------------------------#
 
-# N.B. doesn't matter if run as sudo or not
+# NB: doesn't matter if run as sudo or not
+
+# show some progress
+# NB: first call with sudo to ask for password on its own line (aesthetics)
+sudo echo "Uninstalling Asus_L410M_Numpad... "
 
 # stop service now and on reboot
+echo "Stopping service... "
 sudo systemctl stop asus_l410m_numpad
 sudo systemctl disable asus_l410m_numpad
+echo "Done"
 
 # delete files from location
+echo -n "Deleting files... "
 sudo rm -rf /usr/bin/asus_l410m_numpad.py
 sudo rm -rf /lib/systemd/system/asus_l410m_numpad.service
 sudo rm -rf /var/log/asus_l410m_numpad.log
-
-# remove dependencies
-#sudo apt-get remove python3-libevdev
+echo "Done"
 
 # -)
